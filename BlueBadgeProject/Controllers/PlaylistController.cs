29dbd5110204e45d673fd7199573573e7e84f5c7
Jaeduh <jada.g1210@gmail.com>
2020-03-05 16:26:19 -0500

    jadas playlist controller

--- BlueBadgeProject/Controllers/PlaylistController.cs
+++ BlueBadgeProject/Controllers/PlaylistController.cs
@@ -1,11 +1,59 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using OurTunes.Data;
+using OurTunes.Model;
+using OurTunes.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace BlueBadgeProject.Controllers
 {
-    public class PlaylistController
+    [Authorize]
+    public class PlaylistController : ApiController
     {
+        //GET ALL
+        public IHttpActionResult Get()
+        {
+            PlaylistServices playlistServices = new PlaylistServices();
+            var playlists = playlistServices.GetPlaylists();
+            return Ok(playlists);
+        }
+
+        //POST
+        public IHttpActionResult Post(PlaylistCreate playlist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePlaylistService();
+
+            if (!service.CreatePlaylist(playlist))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+        private PlaylistServices CreatePlaylistService()
+        {
+            var playlistServices = new PlaylistServices();
+            return playlistServices;
+        }
+
+        //PUT
+        public IHttpActionResult Put(PlaylistEdit playlist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePlaylistService();
+
+            if (!service.UpdatePlaylist(playlist))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
