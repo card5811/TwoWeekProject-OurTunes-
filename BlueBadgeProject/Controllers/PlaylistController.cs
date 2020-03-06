7f932152bd5592dc2acc355cd0b53614fc3d2b75
craig dunlap <cadunlap3234@gmail.com>
2020-03-05 21:24:51 -0500

    fixing my playlistcontroller

--- BlueBadgeProject/Controllers/PlaylistController.cs
+++ BlueBadgeProject/Controllers/PlaylistController.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNet.Identity;
+using OurTunes.Data;
+using OurTunes.Model;
+using OurTunes.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Http;
 
 namespace BlueBadgeProject.Controllers
@@ -5,8 +13,47 @@ namespace BlueBadgeProject.Controllers
     [Authorize]
     public class PlaylistController : ApiController
     {
+        //GET ALL
+        public IHttpActionResult Get()
+        {
+            PlaylistServices playlistServices = new PlaylistServices();
+            var playlists = playlistServices.GetPlaylists();
+            return Ok(playlists);
+        }
 
+        //POST
+        public IHttpActionResult Post(PlaylistCreate playlist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
+            var service = CreatePlaylistService();
+
+            if (!service.CreatePlaylist(playlist))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+        private PlaylistServices CreatePlaylistService()
+        {
+            var playlistServices = new PlaylistServices();
+            return playlistServices;
+        }
+
+        //PUT
+        public IHttpActionResult Put(PlaylistEdit playlist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePlaylistService();
+
+            if (!service.UpdatePlaylist(playlist))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
-
 }
